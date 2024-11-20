fa768ec4507b1689a8f9cd9fd4dc5ccda8e8756a
Jay <151044640+BearCubsLOL@users.noreply.github.com>
2024-11-20 12:17:27 -0500

    Started A CameraShake

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -1,34 +1,46 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
 {
     [SerializeField] private float playerSpeed = 5f;
+    [SerializeField] private float cameraShakeSpeed = 2f;
     [SerializeField] private GameObject player;
-    [SerializeField] private bool isWalking = true;
+    [SerializeField] private GameObject playerCamera;
+    [SerializeField] private bool isWalking = false;
 
     void Walk()
     {
         if (Input.GetKey(KeyCode.W))
         {
+            isWalking = true;
             player.transform.position += new Vector3(0f, 0f, playerSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
+            isWalking = true;
             player.transform.position -= new Vector3(0f, 0f, playerSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
+            isWalking = true;
             player.transform.position -= new Vector3(playerSpeed * Time.deltaTime, 0f, 0f);
         }
         if (Input.GetKey(KeyCode.D))
         {
+            isWalking = true;
             player.transform.position += new Vector3(playerSpeed * Time.deltaTime, 0f, 0f);
         }
     }
 
-    void CameraShake()
+    private IEnumerator CameraShake()
     {
-
+        while (isWalking)
+        {
+            playerCamera.transform.position += new Vector3(0f, cameraShakeSpeed * Time.deltaTime, 0f);
+            yield return new WaitForSeconds(0.5f);
+            playerCamera.transform.position -= new Vector3(0f, cameraShakeSpeed * Time.deltaTime, 0f);
+        }
     }
 
     void Update()
